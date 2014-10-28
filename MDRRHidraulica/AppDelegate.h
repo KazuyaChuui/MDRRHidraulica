98bb113f128d0be569539457b06fdb7dda7e5666
JesusRuiz <TheDarkSideOfYisus@users.noreply.github.com>
2014-10-28 12:02:45 -0700

    Initial Commit

---
+++ MDRRHidraulica/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MDRRHidraulica
+//
+//  Created by Jesus Ruiz on 10/28/14.
+//  Copyright (c) 2014 itesm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
